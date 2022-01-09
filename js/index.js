c9d327393d443f5b40a78e69ea3dd0dc1507c03a
KKharidev <kkharidev@gmail.com>
2022-01-09 22:35:09 +0530

    navbar

--- js/index.js
+++ js/index.js
@@ -18,3 +18,36 @@ for(i=0;i<accordian.length;i++){
     })
 }
 
+
+$(document).ready(function () {
+  $("#submit-form").validate({
+      rules: {
+          name: {
+              required: true,
+          },
+          email: {
+              email: true,
+          },
+          message: {
+              required: true,
+              minlength: 10,
+          },
+      },
+
+      submitHandler: function (form) {
+          $.ajax({
+              url: "https://script.google.com/macros/s/AKfycbznIGAyNRAIHVwpDmU6GGwm4k9uehMjJnJrZ5k9YMKoQwgquB3mpu1IiQPuqqwn5xJ_/exec",
+              data: $("#submit-form").serialize(),
+              method: "post",
+              success: function (response) {
+                  alert("Form submitted successfully");
+                  window.location.reload();
+                  //window.location.href="https://google.com"
+              },
+              error: function (err) {
+                  alert("Something Error");
+              },
+          });
+      },
+  });
+});
