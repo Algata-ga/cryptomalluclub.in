c1481bb20448a124f106d656ed9d5870a6dc1e29
Alfred Jophy <alfredjophy@protonmail.com>
2022-01-10 21:08:30 +0530

    bye bye jquery!!!!

--- js/ama.js
+++ js/ama.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
-    $(".hamburger").click(function(){
-      $(".nav-links").toggleClass("open");
-    });
-  });
+function ready(fn) {
+    if (document.readyState != "loading") {
+        fn();
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+ready(function () {
+    document.getElementById("hamburger").addEventListener(
+        "click",
+        function () {
+            document.getElementById("nav-links").classList.toggle("open");
+        },
+        false
+    );
+});
+
--- js/index.js
+++ js/index.js
@@ -1,53 +1,53 @@
-$(document).ready(function(){
-    $(".hamburger").click(function(){
-      $(".nav-links").toggleClass("open");
-    });
-  });
+function ready(fn) {
+    if (document.readyState != "loading") {
+        fn();
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
 
-
-  
-
-
-
+ready(function () {
+    document.getElementById("hamburger").addEventListener(
+        "click",
+        function () {
+            document.getElementById("nav-links").classList.toggle("open");
+        },
+        false
+    );
+});
 
 const accordian = document.getElementsByClassName("contents");
 
-for(i=0;i<accordian.length;i++){
-    accordian[i].addEventListener('click',function(){
-        this.classList.toggle('active')
-    })
+for (i = 0; i < accordian.length; i++) {
+    accordian[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+    });
 }
 
-
-$(document).ready(function () {
-  $("#submit-form").validate({
-      rules: {
-          name: {
-              required: true,
-          },
-          email: {
-              email: true,
-          },
-          message: {
-              required: true,
-              minlength: 10,
-          },
-      },
-
-      submitHandler: function (form) {
-          $.ajax({
-              url: "https://script.google.com/macros/s/AKfycbznIGAyNRAIHVwpDmU6GGwm4k9uehMjJnJrZ5k9YMKoQwgquB3mpu1IiQPuqqwn5xJ_/exec",
-              data: $("#submit-form").serialize(),
-              method: "post",
-              success: function (response) {
-                  alert("Form submitted successfully");
-                  window.location.reload();
-                  //window.location.href="https://google.com"
-              },
-              error: function (err) {
-                  alert("Something Error");
-              },
-          });
-      },
-  });
+ready(function () {
+    document.getElementById("submit-form").addEventListener(
+        "submit",
+        function (form) {
+            const request = new XMLHttpRequest();
+            request.open(
+                "POST",
+                "https://script.google.com/macros/s/AKfycbznIGAyNRAIHVwpDmU6GGwm4k9uehMjJnJrZ5k9YMKoQwgquB3mpu1IiQPuqqwn5xJ_/exec",
+                true
+            );
+            request.setRequestHeader(
+                "Content-Type",
+                "application/x-www-form-urlencoded; charset=UTF-8"
+            );
+            const data = document.getElementById("submit-form").serialize();
+            try {
+                request.send(data);
+                alert("Sent successfully!");
+                window.location.reload();
+            } catch (err) {
+                alert("Sending failed.");
+                window.location.reload();
+            }
+        },
+        false
+    );
 });
