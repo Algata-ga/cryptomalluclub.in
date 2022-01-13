c4b3ef0e9b0c6aeb78381d3d1917d910cbf640da
Alfred Jophy <alfredjophy@protonmail.com>
2022-01-13 11:31:20 +0530

    cleanup

--- dist/ama.996a1a7e.js
+++
@@ -1,2 +0,0 @@
-function ready(e){"loading"!=document.readyState?e():document.addEventListener("DOMContentLoaded",e)}ready((function(){document.getElementById("hamburger").addEventListener("click",(function(){document.getElementById("nav-links").classList.toggle("open")}),!1)}));
-//# sourceMappingURL=ama.996a1a7e.js.map
